2e8adb82944262d177bc1c7114410f601035d396
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-04 14:02:04 +0530

    simple add using a class

--- calculator.java
+++ calculator.java
@@ -1,8 +1,11 @@
 public class calculator {
-    public static void main(String[] args) {
-        int a=5;
-        int b=10;   
-        int c=a+b;
+    public void add(int a, int b) {
+        int c = a + b;
         System.out.println("The sum of a and b is: " + c);
     }
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        calc.add(5, 1);
+    }
 }
+
