8c6d63a523c47f45ed9c4b05a8ec8f38a3d0dfbf
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-04 15:15:03 +0530

    getting user input for addition

--- calculator.java
+++ calculator.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
 public class calculator {
     public void add(int a, int b) {
         int c = a + b;
         System.out.println("The sum of a and b is: " + c);
     }
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
         calculator calc = new calculator();
-        calc.add(5, 1);
+        calc.add(num1, num2);
     }
 }
 
