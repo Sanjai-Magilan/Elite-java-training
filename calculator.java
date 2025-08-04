c886be2e787ab8dbf5e50e58b8c929733bebf0d2
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-04 15:19:26 +0530

    added subraction and multiplication

--- calculator.java
+++ calculator.java
@@ -4,6 +4,14 @@ public class calculator {
         int c = a + b;
         System.out.println("The sum of a and b is: " + c);
     }
+    public void sub(int a, int b){
+        int c = a - b;
+        System.out.println("the difference of a and b is: " + c);
+    }
+    public void mul(int a, int b){
+        int c = a * b;
+        System.out.println("the product of a and b is: " + c);
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter first number: ");
@@ -12,6 +20,8 @@ public class calculator {
         int num2 = input.nextInt();
         calculator calc = new calculator();
         calc.add(num1, num2);
+        calc.sub(num1, num2);
+        calc.mul(num1, num2);
     }
 }
 
