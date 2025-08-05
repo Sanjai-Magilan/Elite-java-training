d8bd0b6980e7730bcfa3f838da531dc1b6726255
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-05 16:37:28 +0530

    palindrom in "For loop"

---
+++ ForLoops/palindrom.java
@@ -0,0 +1,14 @@
+package ForLoops;
+import java.util.*;
+public class palindrom {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a num : ");
+        int temp =0,tem;
+        for(int num = input.nextInt();num>0;num/=10){
+            tem=num%10;
+            temp=temp*10+tem;
+        }input.close();
+        System.out.println("palindrom of the given num : "+temp);
+    }
+}
