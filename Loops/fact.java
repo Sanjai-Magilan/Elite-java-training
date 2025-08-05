da92805999c755bdc9cd20e3f8df91d50b2882f2
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-05 12:28:26 +0530

    factorial

---
+++ Loops/fact.java
@@ -0,0 +1,16 @@
+package Loops;
+import java.util.*;
+public class fact {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int num = input.nextInt();
+        int fac=1;
+        while(num>0){
+            fac*=num;
+            num--;
+        }
+        System.out.println("Factorial = "+ fac);
+        input.close();
+    }
+}
