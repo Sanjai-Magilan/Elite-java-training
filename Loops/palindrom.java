9c8146d4c8e8466acaee12db98c42211744305d7
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-05 14:48:22 +0530

    palindrom

---
+++ Loops/palindrom.java
@@ -0,0 +1,18 @@
+package Loops;
+import java.util.*;
+
+public class palindrom {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int num = input.nextInt();
+        int a,b=0;
+        while(num>0){
+            a = num%10;
+            b=b*10+a;
+            num/=10;
+        }
+        System.out.println("palindrom of the given num is : "+b);
+        input.close();
+    }
+}
