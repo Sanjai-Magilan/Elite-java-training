31744b702501f387261429f535847a378e0f524b
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-05 15:50:11 +0530

    armstrong number

---
+++ Loops/armstrong.java
@@ -0,0 +1,21 @@
+package Loops;
+import java.util.*;
+public class armstrong {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int a,b=0,num,mun;
+        System.out.print("Enter a num: ");
+        num = input.nextInt();
+        mun = num;
+        while (num>0){
+            a=num%10;
+            b +=(a*a*a);
+            num/=10;
+        } 
+        if (b==mun)
+            System.out.println("Given num is armstrong num: "+mun);
+        else
+            System.out.println("Given num is not an armstrong num");
+        input.close();
+    }
+}
