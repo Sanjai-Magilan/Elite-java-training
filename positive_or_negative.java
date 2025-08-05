84f95174b2a1e5ee05add4698159b92d8f7119d9
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-05 10:19:12 +0530

    positive or negative and week days using switch case

--- positive_or_negative.java
+++ positive_or_negative.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class odd_even {
+public class positive_or_negative {
     public static void main(String[] args){
         Scanner input= new Scanner(System.in);
         System.out.print("Enter a num: ");
