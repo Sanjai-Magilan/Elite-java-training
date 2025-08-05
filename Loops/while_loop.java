145cc9dc08f4066851e5c8902f143d3ec47e7aa6
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-05 12:17:23 +0530

    while loop (sum of n numbers)

---
+++ Loops/while_loop.java
@@ -0,0 +1,16 @@
+package Loops;
+import java.util.*;
+public class while_loop {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int add=0,i=1,num = input.nextInt();
+        while(i<=num){
+            add+=i;
+            i++;
+        }
+        System.out.println("Sum = "+add);
+        input.close();
+    }
+}
+ 
