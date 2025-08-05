d83fc826b424f542d5ea292c2c3d60e127301d8e
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-05 10:26:08 +0530

    odd or even

---
+++ odd_or_even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class odd_or_even {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        if (num % 2 == 0 && num !=0){
+            System.out.println("Given num is even");
+        }else if(num %2 != 0 ){
+            System.out.println("Given num is odd");
+        }else{
+            System.out.println("Given num is zero");
+        }
+    }
+}
