69cdfc060685af6129315f08a874f880f1d7bc2b
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-06 12:11:13 +0530

    2D Array

---
+++ Arrays/twoD_array.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.Scanner;
+public class twoD_array {
+    public static  void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int arr[][] = new int[3][3];
+        System.out.println("Enter the value for array down below");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                arr[i][j]= input.nextInt();
+            }
+        }
+        input.close();
+        System.out.println("the 2D array values are");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
