1868a455d17bd734a13f152035cd6f002b2e1a5c
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-06 12:45:47 +0530

    agian 2D update

--- Arrays/twoD_array.java
+++ Arrays/twoD_array.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class twoD_array {
     public static  void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter number of rows : ");
+        System.out.print("Enter number of rows : ");
         int row = input.nextInt();
-        System.out.println("Enter number of colum : ");
+        System.out.print("Enter number of colum : ");
         int col = input.nextInt();
         int arr[][] = new int[row][col];
         System.out.println("Enter the value for array down below");
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 arr[i][j]= input.nextInt();
             }
         }
         input.close();
         System.out.println("the 2D array values are");
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 System.out.print(arr[i][j]+" ");
             }
             System.out.println();
