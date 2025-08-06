ee55cf0e4d3329ee5c0af90a0a8892fa67bb4473
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-06 16:05:36 +0530

    Abstraction

---
+++ Abstraction/Abs.java
@@ -0,0 +1,20 @@
+package Abstraction;
+abstract class sample{
+    void add(int a,int b){
+        System.out.println("Add = "+(a+b));
+    }
+    abstract void sub(int a,int b);
+}
+
+class samp extends sample{
+   void sub(int a,int b){
+        System.out.println("Sub = "+(a-b));
+    }
+}
+public class Abs {
+    public static void main(String[] args){
+        samp obj = new samp();
+        obj.add(6, 20);
+        obj.sub(6, 20);
+    }
+}
