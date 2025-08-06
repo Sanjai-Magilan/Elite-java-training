733a8f190444adbfa80f6711a1bc797d06b9a9f4
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-06 12:43:19 +0530

    2D update

--- Arrays/twoD_array.java
+++ Arrays/twoD_array.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 public class twoD_array {
     public static  void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int arr[][] = new int[3][3];
+        System.out.println("Enter number of rows : ");
+        int row = input.nextInt();
+        System.out.println("Enter number of colum : ");
+        int col = input.nextInt();
+        int arr[][] = new int[row][col];
         System.out.println("Enter the value for array down below");
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
