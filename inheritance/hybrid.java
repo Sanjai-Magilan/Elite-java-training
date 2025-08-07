fc3fbc1cbf226cb087a38313f574a4608c58b4b8
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-07 16:29:33 +0530

    hybrid inheritance

---
+++ inheritance/hybrid.java
@@ -0,0 +1,37 @@
+package inheritance;
+class base{
+    void SoCalledMethod(){
+        System.out.println("so called print statement");
+    }
+}
+
+class branch8 extends base{
+    void function(){
+        System.out.println("branch 0 over over!");
+    }
+}
+
+class branch9 extends base{
+    void fun(){
+        System.out.println("branch 1 over over!");
+    }
+}
+
+class unknown extends branch9{
+    void known (){
+        System.out.println("known is unknown and unknown is known");
+    }
+}
+
+public class hybrid {
+     public static void main(String[] args){
+        branch0 obj = new branch0();
+        unknown jbo = new unknown();
+        obj.SoCalledMethod();
+        obj.function();
+        jbo.SoCalledMethod();
+        jbo.fun();
+        jbo.known();
+    }
+}
+
