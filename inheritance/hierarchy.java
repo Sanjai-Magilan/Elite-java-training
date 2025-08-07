0368356db73f012509ec47a15745d37004645f63
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-07 16:10:19 +0530

    hierarchy inheritance

---
+++ inheritance/hierarchy.java
@@ -0,0 +1,29 @@
+package inheritance;
+class base{
+    void SoCalledMethod(){
+        System.out.println("so called print statement");
+    }
+}
+
+class branch0 extends base{
+    void function(){
+        System.out.println("branch 0 over over!");
+    }
+}
+
+class branch1 extends base{
+    void fun(){
+        System.out.println("branch 1 over over!");
+    }
+}
+
+public class hierarchy{
+    public static void main(String[] args){
+        branch0 obj = new branch0();
+        branch1 jbo= new branch1();
+        obj.SoCalledMethod();
+        obj.function();
+        jbo.SoCalledMethod();
+        jbo.fun();
+    }
+}
