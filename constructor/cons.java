6863a92c46f303300e9bba16b86a75d1dd0f089c
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-07 12:31:32 +0530

    constructor

---
+++ constructor/cons.java
@@ -0,0 +1,11 @@
+package constructor;
+class DoesNothing{
+    DoesNothing() {
+        System.out.println("Nothing");
+    }
+}
+public class cons {
+    public static void main(String[] args){
+        DoesNothing ntg = new DoesNothing();
+    }
+}
