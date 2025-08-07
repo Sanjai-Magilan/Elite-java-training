598aa3a00cd33712c91330d6b41f399af8aa1212
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-07 15:30:36 +0530

    single inheritance

---
+++ inheritance/single.java
@@ -0,0 +1,30 @@
+package inheritance;
+class parent{
+    parent(){
+        System.out.println("i'm parent");
+    }
+    void gen0(){
+        System.out.println("dad's assessets");
+    }
+}
+
+class son extends parent{
+    void gen1(){
+        System.out.println("son's assessets");
+    }
+}
+
+class grandson extends son{
+    void gen2(){
+        System.out.println("grandson's assessets");
+    }
+}
+
+public class single {
+    public static void main(String[] args){
+        grandson obj = new grandson();
+        obj.gen0();
+        obj.gen1();
+        obj.gen2();
+    }    
+}
