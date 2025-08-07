4e203cfdbe3ed5e70e781fdb883381bff4c870bd
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-07 12:19:24 +0530

    encapsulation

---
+++ encap/encap.java
@@ -0,0 +1,28 @@
+package encap;
+class data{
+    private int age;
+    private String name;
+    
+    void setname(String name){
+        this.name=name;
+    }
+    void setage(int age){
+        this.age=age;
+    }
+    String getname(){
+        return name;
+    }
+    int getage(){
+        return age;
+    }
+
+}
+public class encap {
+    public static void main(String[] args){
+        data info= new data();
+        info.setname("farfella");
+        info.setage(19);
+        System.out.println(info.getname());
+        System.out.println(info.getage());
+    }
+}
