1e65724253f16e0e928c555801f847b1384980bb
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-07 12:37:40 +0530

    constructor (i just found that i can call constructor without creating an object)

--- constructor/cons.java
+++ constructor/cons.java
@@ -3,9 +3,14 @@ class DoesNothing{
     DoesNothing() {
         System.out.println("Nothing");
     }
+    void something(){
+        System.out.println("Printing smgth");
+    }
 }
 public class cons {
     public static void main(String[] args){
-        DoesNothing ntg = new DoesNothing();
+        DoesNothing ntg = new DoesNothing();// creating an object 
+        new DoesNothing();//call constructor only not creating an obj
+        ntg.something();
     }
 }
