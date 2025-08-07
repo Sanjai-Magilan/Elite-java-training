fc32f6378e8c09a1737649180f7afa42a72bdb4f
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-07 09:28:16 +0530

    sum and product

---
+++ test/SumAndProduct.java
@@ -0,0 +1,22 @@
+package test;
+import java.util.Scanner;
+
+public class SumAndProduct {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a num : ");
+        int num = input.nextInt();
+        input.close();
+        int a,sum =0, product=1;
+        while(num>0){
+            a=num%10;
+            sum+=a;
+            product*=a;
+            num/=10;
+        }
+        if(sum==product)
+            System.out.println("true");
+        else
+            System.out.println("false");
+    }
+}
