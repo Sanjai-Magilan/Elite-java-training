4bdc52219f0bb77e11e883c7e2c600acb670a9f5
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-08 16:02:53 +0530

    Polymorphism over raiding

---
+++ OOP/Polymorphism/overraiding.java
@@ -0,0 +1,20 @@
+package OOP.Polymorphism;
+
+class sum {
+    void add(int a,int b, int c){
+        System.out.println("sum of three num is : "+(a+b+c));
+    }
+
+    void add(int a,int b){
+        System.out.println("sum of two num is : "+(a+b));
+    }
+}
+
+
+public class overraiding {
+    public static void main(String[] args){
+        sum obj=new sum();
+        obj.add(1,1,1);
+        obj.add(2,2);
+    }    
+}
