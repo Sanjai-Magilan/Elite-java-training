777d2aa2f8b2324bdea72db5550aac7f7d78267d
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-09 12:04:52 +0530

    yeah baby, yeah, c'mon baby, yes ! .Dynamic Spiral is working yes !

--- test/SpiralDynamic.java
+++ test/SpiralDynamic.java
@@ -16,19 +16,26 @@ public class SpiralDynamic {
                 arr[i][j]=input.nextInt();
             }
         }
+        System.out.println();
         input.close();
-
-        for(int j=0;j<col;j++){
-            System.out.print(arr[0][j]+" ");
-        }
-        for (int i=1;i<row;i++){
-            System.out.print(arr[i][col-1]+" ");
-        }
-        for(int j=col-2;j>=0;j--){
-            System.out.print(arr[row-1][j]+" ");
-        }
-        for(int j=0;j<col-1;j++){
-            System.out.print(arr[1][j]+" ");
+        int top = 0, bottom =row-1, right=col-1, left=0;
+        while(top <=row && bottom >=0 && left<=col && right >=0){
+            for(int j=left;j<=right;j++){
+                System.out.print(arr[top][j]+" ");
+            }
+            for (int i=top+1;i<=bottom;i++){
+                System.out.print(arr[i][right]+" ");
+            }
+            for(int j=right-1;j>=left;j--){
+                System.out.print(arr[bottom][j]+" ");
+            }
+            for(int i=bottom-1;i>top;i--){
+                System.out.print(arr[i][left]+" ");
+            }
+           top++;
+           left++;
+           right--;
+           bottom--;
         }
     }
 }
