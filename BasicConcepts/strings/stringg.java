6b6f171b4b3c7bdfc3d3b4532bec1cbc5d82eba8
Sanjai-Magilan <sanjai6115@gmail.com>
2025-08-18 12:06:18 +0530

    string functions

---
+++ BasicConcepts/strings/stringg.java
@@ -0,0 +1,17 @@
+package BasicConcepts.strings;
+
+public class stringg {
+    public static void main(String[] args){
+        String str="Hello World";
+        System.out.println("length of the string : "+str.length());
+        String s=String.valueOf(str.charAt(4));
+        System.out.println("At index 4 : "+s);
+        System.out.println("Upper Case Char : "+s.toUpperCase());
+        boolean empt = str.isEmpty();
+        System.out.println("Is the string empty : "+empt);
+        String dummy = "  hello world  ";
+        System.out.println("trimmed : "+dummy.trim());
+        System.out.println("front trimmed : "+dummy.stripLeading());
+        System.out.println("back trimmed : "+dummy.stripTrailing());
+    }
+}
